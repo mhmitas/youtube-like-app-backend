d66c2fea95c2057fffffc90379154e38907f5802
Mh Mitas <mahfuzulmitas@gmail.com>
2024-07-01 14:02:08 +0600

    commit - register route create

--- src/app.js
+++ src/app.js
@@ -13,3 +13,11 @@ app.use(express.json({ limit: '16kb' }))
 // app.use(express.urlencoded())
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
